1b47c57d726989c1a627af068470bde1cbd0ed0d
Dylan Jacobs <78403395+dylan-jacobs@users.noreply.github.com>
2025-01-18 14:04:08 -0800

    Update alpaca_tester.py

--- alpaca_tester.py
+++ alpaca_tester.py
@@ -25,6 +25,7 @@ from dotenv import load_dotenv
 
 # ALPACA API Info for fetching data, portfolio, etc. from Alpaca
 BASE_URL = os.getenv("BASE_URL")
+BASE_URL = "https://paper-api.alpaca.markets"
 ALPACA_API_KEY = os.getenv("ALPACA_API_KEY")
 ALPACA_SECRET_KEY = os.getenv("ALPACA_SECRET_KEY")
 TICKER = 'SPY'
